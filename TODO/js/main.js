f84777905c11606522d55a4deb1499a434505e79
HEENEW <heewoo_z@naver.com>
2022-04-27 23:04:06 +0900

    second

--- TODO/js/main.js
+++ TODO/js/main.js
@@ -10,31 +10,31 @@ const likeButtons = document.querySelectorAll(".like");
 function addItem(){
     if(inputText.value.trim() === "") return;
 
-    //like
+    //like  하트
     const like = document.createElement("span");
     const likeIcon = document.createElement("i");
-    like.classList.add("like");
+    like.classList.add("like");  // 클래스를 추가
     likeIcon.classList.add("material-symbols-outlined");
     likeIcon.innerText="favorite"
     like.appendChild(likeIcon); 
 
-    //item
+    //item  내용
     const item = document.createElement("span");
     item.classList.add("item");
     item.innerText = inputText.value;
 
 
-    //manege
-    const manage = document.createElement("span");
-    const checkIcon = document.createElement("i");
-    const closeIcon = document.createElement("i");
+    //manege  체크, 엑스
+    const manage = document.createElement("span"); 
+    const checkIcon = document.createElement("i");  // 체크 아이콘
+    const closeIcon = document.createElement("i");  // 엑스 아이콘 생성
     checkIcon.classList.add("material-symbols-outlined","check");
     closeIcon.classList.add("material-symbols-outlined","close");
     checkIcon.innerText = "check"
     closeIcon.innerText = "close"
     manage.classList.add("manage");
     manage.appendChild(checkIcon);
-    manage.appendChild(closeIcon);
+    manage.appendChild(closeIcon); // manage에 checkIcon과 closeIcon 넣음
 
 
     const li = document.createElement("li");  // html요소를 하나 만들겠다
@@ -60,13 +60,13 @@ function addItem(){
     li.appendChild(like)
     li.appendChild(item);
     li.appendChild(manage);
-    list.appendChild(li);  // 자식 요소에 엘리멘트를 넣을때 사용하는 메서드
+    list.appendChild(li);  // ul태그(list) 안에 자식 요소로 엘리멘트를 넣겠다
 
-    inputText.value = "";
+    inputText.value = "";  // inputText 내용 초기화
     inputText.focus()
 }
 
-inputText.addEventListener("keypress", e => {
+inputText.addEventListener("keypress", e => {  // 인자를 이벤트로 넘겨받음
     if(e.keyCode === 13){
         addItem();
     }
@@ -80,4 +80,4 @@ likeButtons.forEach(like=>{
 })
 
 
-addButton.addEventListener("click", addItem);
+addButton.addEventListener("click", addItem);  // 버튼 클릭시 addItem에 inputText의 value값이 들어감
