cba53855f827c71097ce1151ae139ada2f6739ac
HEENEW <heewoo_z@naver.com>
2022-05-10 23:50:22 +0900

    123

--- vue-corona-app/src/components/corona/InternationalCases.vue
+++ vue-corona-app/src/components/corona/InternationalCases.vue
@@ -3,8 +3,20 @@
 </template>
 
 <script>
+import coronaMixin from "@/mixins/coronaMixin"
 export default {
   name: "InternationalCases",
+  mixins: [coronaMixin],
+  mounted() {
+    this.fetchCases()
+  },
+  methods: {
+    async fetchCases(){
+      const url="https://api.covid19api.com/summary"
+      const summary = await this.fetchCases("get", url)
+      console.log(summary, "res")
+    }
+  },
 };
 </script>
 
---
+++ vue-corona-app/src/mixins/coronaMixin.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+const coronaMixin = {
+    methods:{
+        // method: get방식인지 post방식인지 구분하기 위함
+        async fetchData(method, url, params){
+            const res = await axios[method](url, {params})
+            return res.data;
+        }
+    }
+}
+
+export default coronaMixin;
